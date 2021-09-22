39daa68cbdc0962138354dc4cfb58c39cb310231
t1agob <tbarbos@hotmail.com>
2021-09-22 11:04:36 +0100

    created metrics attribute instead of using source generator

--- libraries/src/AWS.Lambda.PowerTools.Metrics/Generator/MetricsGenerator.cs
+++ libraries/src/AWS.Lambda.PowerTools.Metrics/Generator/MetricsGenerator.cs
@@ -7,20 +7,20 @@ namespace AWS.Lambda.PowerTools.Metrics.Generator
     [Generator]
     public class MetricsGenerator : ISourceGenerator
     {
-        private readonly string _metricsAttributeStub = $@"// This code was auto-generated
-using System;
-
-namespace AWS.Lambda.PowerTools.Metrics
-{{
-    [AttributeUsage(AttributeTargets.Method)]
-    public class MetricsAttribute : Attribute
-    {{
-        public MetricsAttribute() {{ }}
-
-        public MetricsAttribute(string serviceNamespace, string serviceName) {{ }}
-    }}
-}}
-";               
+//         private readonly string _metricsAttributeStub = $@"// This code was auto-generated
+// using System;
+//
+// namespace AWS.Lambda.PowerTools.Metrics
+// {{
+//     [AttributeUsage(AttributeTargets.Method)]
+//     public class MetricsAttribute : Attribute
+//     {{
+//         public MetricsAttribute() {{ }}
+//
+//         public MetricsAttribute(string serviceNamespace, string serviceName) {{ }}
+//     }}
+// }}
+// ";               
         
         public void Initialize(GeneratorInitializationContext context)
         {
@@ -30,7 +30,7 @@ namespace AWS.Lambda.PowerTools.Metrics
 
         public void Execute(GeneratorExecutionContext context)
         {
-            context.AddSource("MetricsAttribute", _metricsAttributeStub);    
+            // context.AddSource("MetricsAttribute", _metricsAttributeStub);    
             
             if (context.SyntaxReceiver is not SyntaxReceiver syntaxReceiver)                            
             {                                                                                           
