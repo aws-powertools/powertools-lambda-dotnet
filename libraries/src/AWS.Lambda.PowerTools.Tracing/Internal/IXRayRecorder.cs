24dbb1cd3547258f529ccbf08023547f46b7061c
Amir Khairalomoum <amirkaws@amazon.com>
2021-12-10 01:06:24 +0000

    Add tracing utilities

--- libraries/src/AWS.Lambda.PowerTools.Tracing/Internal/IXRayRecorder.cs
+++ libraries/src/AWS.Lambda.PowerTools.Tracing/Internal/IXRayRecorder.cs
@@ -1,13 +1,21 @@
 using System;
+using Amazon.XRay.Recorder.Core.Internal.Emitters;
+using Amazon.XRay.Recorder.Core.Internal.Entities;
+using Amazon.XRay.Recorder.Core.Strategies;
 
 namespace Amazon.Lambda.PowerTools.Tracing.Internal
 {
     public interface IXRayRecorder
     {
+        ISegmentEmitter Emitter { get; }
+        IStreamingStrategy StreamingStrategy { get; }
+        
         void BeginSubsegment(string name, DateTime? timestamp = null);
         void SetNamespace(string value);
         void AddAnnotation(string key, object value);
         void AddMetadata(string nameSpace, string key, object value);
         void EndSubsegment();
+        Entity GetEntity();
+        void SetEntity(Entity entity);
     }
 }
