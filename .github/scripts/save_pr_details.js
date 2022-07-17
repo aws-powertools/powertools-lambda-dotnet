dc46efd0c019589b733fcc2d0263093e59132007
Stephen Liedig <sliedig@amazon.com>
2022-07-18 00:20:38 +0800

    feat: updated record_pr action

---
+++ .github/scripts/save_pr_details.js
@@ -0,0 +1,13 @@
+module.exports = async ({github, context, core}) => {
+    const fs = require('fs');
+    const filename = "pr.txt";
+
+    try {
+        fs.writeFileSync(`./${filename}`, JSON.stringify(context.payload));
+
+        return `PR successfully saved ${filename}`
+    } catch (err) {
+        core.setFailed("Failed to save PR details");
+        console.error(err);
+    }
+}
