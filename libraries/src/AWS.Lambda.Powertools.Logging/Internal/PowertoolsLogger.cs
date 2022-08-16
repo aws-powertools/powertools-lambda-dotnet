7324009c86064674ce05a5374d1570e90c1e1312
Amir Khairalomoum <amirkaws@amazon.com>
2022-08-16 14:36:24 +0100

    minor changes in comments and formatting

--- libraries/src/AWS.Lambda.Powertools.Logging/Internal/PowertoolsLogger.cs
+++ libraries/src/AWS.Lambda.Powertools.Logging/Internal/PowertoolsLogger.cs
@@ -55,7 +55,7 @@ internal sealed class PowertoolsLogger : ILogger
     private LoggerConfiguration _currentConfig;
     
     /// <summary>
-    ///     The json serializer options
+    ///     The JsonSerializer options
     /// </summary>
     private JsonSerializerOptions _jsonSerializerOptions;
 
