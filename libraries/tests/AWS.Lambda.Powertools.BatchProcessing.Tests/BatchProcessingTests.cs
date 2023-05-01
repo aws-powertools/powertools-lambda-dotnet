b1c8a99fdcc780650421b7fdd610fd105c82eada
Lasse Christiansen <lachriz@amazon.com>
2023-05-01 22:04:41 +0200

    Added unit tests for SQS batch processing logic (standard vs fifo queues).

--- libraries/tests/AWS.Lambda.Powertools.BatchProcessing.Tests/BatchProcessingTests.cs
+++ libraries/tests/AWS.Lambda.Powertools.BatchProcessing.Tests/BatchProcessingTests.cs
@@ -13,6 +13,13 @@
  * permissions and limitations under the License.
  */
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Amazon.Lambda.SQSEvents;
+using AWS.Lambda.Powertools.BatchProcessing.Sqs;
+using Moq;
 using Xunit;
 
 namespace AWS.Lambda.Powertools.BatchProcessing.Tests
@@ -21,11 +28,91 @@ namespace AWS.Lambda.Powertools.BatchProcessing.Tests
     public class BatchProcessingTests
     {
         [Fact]
-        public void OnEntry_Test()
+        public async Task SqsBatchProcessor_StandardQueue_ContinueOnBatchItemFailure()
         {
             // Arrange
+            const string eventSourceArn = "arn:aws:sqs:eu-west-1:123456789012:test-queue";
+            var @event = new SQSEvent
+            {
+                Records = new List<SQSEvent.SQSMessage>
+                {
+                    new()
+                    {
+                        EventSourceArn = eventSourceArn,
+                        MessageId = "1",
+                    },
+                    new()
+                    {
+                        EventSourceArn = eventSourceArn,
+                        MessageId = "2"
+                    },
+                    new()
+                    {
+                        EventSourceArn = eventSourceArn,
+                        MessageId = "3"
+                    }
+                }
+            };
+            var batchProcessor = new SqsBatchProcessor();
+            var recordHandler = new Mock<IRecordHandler<SQSEvent.SQSMessage>>();
+            recordHandler.Setup(x => x.HandleAsync(It.IsAny<SQSEvent.SQSMessage>())).Callback((SQSEvent.SQSMessage x) =>
+            {
+                if (x.MessageId == "1")
+                {
+                    throw new InvalidOperationException();
+                }
+            });
+
             // Act
+            var batchResponse = await batchProcessor.ProcessAsync(@event, recordHandler.Object);
+
             // Assert
+            recordHandler.Verify(x => x.HandleAsync(It.IsIn(@event.Records.AsEnumerable())), Times.Exactly(@event.Records.Count));
+            Assert.Single(batchResponse.BatchItemFailures);
+        }
+
+        [Fact]
+        public async Task SqsBatchProcessor_FifoQueue_StopOnFirstBatchItemFailure()
+        {
+            // Arrange
+            const string eventSourceArn = "arn:aws:sqs:eu-west-1:123456789012:test-queue.fifo";
+            var @event = new SQSEvent
+            {
+                Records = new List<SQSEvent.SQSMessage>
+                {
+                    new()
+                    {
+                        EventSourceArn = eventSourceArn,
+                        MessageId = "1",
+                    },
+                    new()
+                    {
+                        EventSourceArn = eventSourceArn,
+                        MessageId = "2"
+                    },
+                    new()
+                    {
+                        EventSourceArn = eventSourceArn,
+                        MessageId = "3"
+                    }
+                }
+            };
+            var batchProcessor = new SqsBatchProcessor();
+            var recordHandler = new Mock<IRecordHandler<SQSEvent.SQSMessage>>();
+            recordHandler.Setup(x => x.HandleAsync(It.IsAny<SQSEvent.SQSMessage>())).Callback((SQSEvent.SQSMessage x) =>
+            {
+                if (x.MessageId == "1")
+                {
+                    throw new InvalidOperationException();
+                }
+            });
+
+            // Act
+            var batchResponse = await batchProcessor.ProcessAsync(@event, recordHandler.Object);
+
+            // Assert
+            recordHandler.Verify(x => x.HandleAsync(It.IsIn(@event.Records.AsEnumerable())), Times.Once);
+            Assert.Equal(@event.Records.Count, batchResponse.BatchItemFailures.Count);
         }
     }
 }
