9b42b344bc16c489ddbfe2b65ed7c3853f715fcb
Henrique <999396+hjgraca@users.noreply.github.com>
2024-07-11 19:11:57 +0100

    add unit tests for MetricResolutionJsonConverter

---
+++ libraries/tests/AWS.Lambda.Powertools.Metrics.Tests/SerializationTests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Text.Json;
+using Xunit;
+
+namespace AWS.Lambda.Powertools.Metrics.Tests;
+
+[Collection("Sequential")]
+public class SerializationTests
+{
+    [Fact]
+    public void Metrics_Resolution_JsonConverter()
+    {
+        // Arrange
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new MetricResolutionJsonConverter());
+        
+        {
+            var myInt = JsonSerializer.Deserialize<MetricResolution>("1", options);
+            Assert.Equal(MetricResolution.High, myInt);
+            Assert.Equal("1", JsonSerializer.Serialize(myInt, options));
+        }
+        
+        {
+            var myInt = JsonSerializer.Deserialize<MetricResolution>("60", options);
+            Assert.Equal(MetricResolution.Standard, myInt);
+            Assert.Equal("60", JsonSerializer.Serialize(myInt, options));
+        }
+        
+        {
+            var myInt = JsonSerializer.Deserialize<MetricResolution>("0", options);
+            Assert.Equal(MetricResolution.Default, myInt);
+            Assert.Equal("0", JsonSerializer.Serialize(myInt, options));
+        }
+
+        {
+            var myInt = JsonSerializer.Deserialize<MetricResolution>(@"""1""", options);
+            Assert.Equal(MetricResolution.High, myInt);
+            Assert.Equal("1", JsonSerializer.Serialize(myInt, options));
+        }
+        {
+            var myInt = JsonSerializer.Deserialize<MetricResolution>(@"""60""", options);
+            Assert.Equal(MetricResolution.Standard, myInt);
+            Assert.Equal("60", JsonSerializer.Serialize(myInt, options));
+        }
+        {
+            var myInt = JsonSerializer.Deserialize<MetricResolution>(@"""0""", options);
+            Assert.Equal(MetricResolution.Default, myInt);
+            Assert.Equal("0", JsonSerializer.Serialize(myInt, options));
+        }
+    }
+    
+    [Fact]
+    public void Metrics_Resolution_JsonConverter_Exception()
+    {
+        // Arrange
+        var options = new JsonSerializerOptions();
+        options.Converters.Add(new MetricResolutionJsonConverter());
+        
+        // Act
+        Action act = () => JsonSerializer.Deserialize<MetricResolution>("abd", options);
+        
+        // Assert
+        Assert.Throws<JsonException>(act);
+    }
+}
