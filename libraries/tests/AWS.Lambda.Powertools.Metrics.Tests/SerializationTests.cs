7f4d676ba7d07addb52cf9659bf3795fef47f1c8
Henrique <999396+hjgraca@users.noreply.github.com>
2024-07-12 09:59:27 +0100

    more test coverage

--- libraries/tests/AWS.Lambda.Powertools.Metrics.Tests/SerializationTests.cs
+++ libraries/tests/AWS.Lambda.Powertools.Metrics.Tests/SerializationTests.cs
@@ -13,19 +13,25 @@ public class SerializationTests
         // Arrange
         var options = new JsonSerializerOptions();
         options.Converters.Add(new MetricResolutionJsonConverter());
-        
+
+        {
+            var myInt = JsonSerializer.Deserialize<MetricResolution>(1, options);
+            Assert.Equal(MetricResolution.High, myInt);
+            Assert.Equal("1", JsonSerializer.Serialize(myInt, options));
+        }
+
         {
             var myInt = JsonSerializer.Deserialize<MetricResolution>("1", options);
             Assert.Equal(MetricResolution.High, myInt);
             Assert.Equal("1", JsonSerializer.Serialize(myInt, options));
         }
-        
+
         {
             var myInt = JsonSerializer.Deserialize<MetricResolution>("60", options);
             Assert.Equal(MetricResolution.Standard, myInt);
             Assert.Equal("60", JsonSerializer.Serialize(myInt, options));
         }
-        
+
         {
             var myInt = JsonSerializer.Deserialize<MetricResolution>("0", options);
             Assert.Equal(MetricResolution.Default, myInt);
@@ -48,18 +54,27 @@ public class SerializationTests
             Assert.Equal("0", JsonSerializer.Serialize(myInt, options));
         }
     }
-    
+
     [Fact]
     public void Metrics_Resolution_JsonConverter_Exception()
     {
         // Arrange
         var options = new JsonSerializerOptions();
         options.Converters.Add(new MetricResolutionJsonConverter());
+
+        {
+            Action act = () => JsonSerializer.Deserialize<MetricResolution>("1.3", options);
+            Assert.Throws<JsonException>(act);
+        }
         
-        // Act
-        Action act = () => JsonSerializer.Deserialize<MetricResolution>("abd", options);
+        {
+            Action act = () => JsonSerializer.Deserialize<MetricResolution>(@"""1.3""", options);
+            Assert.Throws<JsonException>(act);
+        }
         
-        // Assert
-        Assert.Throws<JsonException>(act);
+        {
+            Action act = () => JsonSerializer.Deserialize<MetricResolution>(@"""abc""", options);
+            Assert.Throws<JsonException>(act);
+        }
     }
 }
