35e613b0d3b22ea304adad0ab0a74509e951e844
Henrique <999396+hjgraca@users.noreply.github.com>
2024-09-02 15:11:14 +0100

    remove comments

--- libraries/src/AWS.Lambda.Powertools.Logging/Internal/Converters/ExceptionPropertyExtractor.cs
+++ libraries/src/AWS.Lambda.Powertools.Logging/Internal/Converters/ExceptionPropertyExtractor.cs
@@ -11,20 +11,12 @@ namespace AWS.Lambda.Powertools.Logging.Internal.Converters;
 /// </summary>
 internal static class ExceptionPropertyExtractor
 {
-    
-    // Note: Leave code comments for future reference
-    
     /// <summary>
     ///     The property extractors
     /// </summary>
     private static readonly Dictionary<Type, Func<Exception, IEnumerable<KeyValuePair<string, object>>>> PropertyExtractors = new()
     {
         { typeof(Exception), GetBaseExceptionProperties },
-        // Add more specific exception types here
-        // { typeof(ArgumentException), GetArgumentExceptionProperties },
-        // { typeof(InvalidOperationException), GetInvalidOperationExceptionProperties },
-        // { typeof(NullReferenceException), GetBaseExceptionProperties },
-        // ... add more as needed
     };
 
     /// <summary>
@@ -35,14 +27,6 @@ internal static class ExceptionPropertyExtractor
     /// <returns></returns>
     public static IEnumerable<KeyValuePair<string, object>> ExtractProperties(Exception exception)
     {
-        // var exceptionType = exception.GetType();
-        
-        // if (PropertyExtractors.TryGetValue(exceptionType, out var extractor))
-        // {
-        //     return extractor(exception);
-        // }
-
-        // If we don't have a specific extractor, use the base Exception extractor
         return GetBaseExceptionProperties(exception);
     }
 
@@ -56,23 +40,5 @@ internal static class ExceptionPropertyExtractor
         yield return new KeyValuePair<string, object>(nameof(Exception.Message), ex.Message);
         yield return new KeyValuePair<string, object>(nameof(Exception.Source), ex.Source);
         yield return new KeyValuePair<string, object>(nameof(Exception.StackTrace), ex.StackTrace);
-        // Add any other properties you want to extract
     }
-
-    // private static IEnumerable<KeyValuePair<string, object>> GetArgumentExceptionProperties(Exception ex)
-    // {
-    //     var argEx = (ArgumentException)ex;
-    //     foreach (var prop in GetBaseExceptionProperties(ex))
-    //     {
-    //         yield return prop;
-    //     }
-    //     yield return new KeyValuePair<string, object>(nameof(ArgumentException.ParamName), argEx.ParamName);
-    // }
-    //
-    // private static IEnumerable<KeyValuePair<string, object>> GetInvalidOperationExceptionProperties(Exception ex)
-    // {
-    //     // InvalidOperationException doesn't have any additional properties
-    //     // but we include it here as an example
-    //     return GetBaseExceptionProperties(ex);
-    // }
 }
