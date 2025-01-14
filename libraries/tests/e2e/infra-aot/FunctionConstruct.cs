c6d300f6f8f599b6a33853c313f26f2d445ca3af
Henrique <999396+hjgraca@users.noreply.github.com>
2025-01-14 11:53:28 +0000

    first aot commit

--- libraries/tests/e2e/infra-aot/FunctionConstruct.cs
+++ libraries/tests/e2e/infra-aot/FunctionConstruct.cs
@@ -10,7 +10,6 @@ public class FunctionConstruct : Construct
 {
     public FunctionConstruct(Construct scope, string id, FunctionConstructProps props) : base(scope, id)
     {
-        var framework = props.Runtime == Runtime.DOTNET_6 ? "net6.0" : "net8.0";
         var distPath = $"{props.DistPath}/deploy_{props.Architecture.Name}_{props.Runtime.Name}.zip";
         _ = new Function(this, id, new FunctionProps
         {
@@ -22,7 +21,7 @@ public class FunctionConstruct : Construct
             Timeout = Duration.Seconds(10),
             Code = Code.FromCustomCommand(distPath,
                 [
-                    $"dotnet-lambda package -pl {props.SourcePath} -o {distPath} -f {framework} -farch {props.Architecture.Name} -cifb public.ecr.aws/sam/build-dotnet8"
+                    $"dotnet-lambda package -pl {props.SourcePath} -o {distPath} -f net8.0 -farch {props.Architecture.Name} -cifb public.ecr.aws/sam/build-dotnet8"
                 ],
                 new CustomCommandOptions
                 {
