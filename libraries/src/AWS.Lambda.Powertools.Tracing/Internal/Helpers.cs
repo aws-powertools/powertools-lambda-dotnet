668ded8f26af3124b26f4839dec8d7c446927944
Henrique <999396+hjgraca@users.noreply.github.com>
2025-01-31 15:59:19 +0000

    Sanitize SegmentName. Show accepted chars in the SegmentName. Add tests

---
+++ libraries/src/AWS.Lambda.Powertools.Tracing/Internal/Helpers.cs
@@ -0,0 +1,23 @@
+using System.Text.RegularExpressions;
+
+namespace AWS.Lambda.Powertools.Tracing.Internal;
+
+/// <summary>
+///    Helper class
+/// </summary>
+public static class Helpers
+{
+    /// <summary>
+    /// Sanitize a string by removing any characters that are not alphanumeric, whitespace, or one of the following: _ . : / % & # = + - @
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static string SanitizeString(string input)
+    {
+        // Define a regular expression pattern to match allowed characters
+        var pattern = @"[^a-zA-Z0-9\s_\.\:/%&#=+\-@]";
+
+        // Replace any character that does not match the pattern with an empty string
+        return Regex.Replace(input, pattern, string.Empty);
+    }
+}
