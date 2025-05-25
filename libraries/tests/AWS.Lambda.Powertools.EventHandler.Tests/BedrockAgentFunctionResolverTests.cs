64d3a618ebc0434f60b8a89038623014658d541a
Henrique <999396+hjgraca@users.noreply.github.com>
2025-05-25 16:18:00 +0100

    add methods that take DI services, match parameters with handler arguments

--- libraries/tests/AWS.Lambda.Powertools.EventHandler.Tests/BedrockAgentFunctionResolverTests.cs
+++ libraries/tests/AWS.Lambda.Powertools.EventHandler.Tests/BedrockAgentFunctionResolverTests.cs
@@ -1,7 +1,9 @@
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Amazon.BedrockAgentRuntime.Model;
+using Amazon.Lambda.Core;
 using Amazon.Lambda.TestUtilities;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AWS.Lambda.Powertools.EventHandler.Tests;
 
@@ -100,7 +102,7 @@ public class BedrockAgentFunctionResolverTests
         // Arrange
         var resolver = new BedrockAgentFunctionResolver();
         resolver.Tool("TestFunction",
-            (input) => new ActionGroupInvocationOutput { Text = $"Hello, {input.Function}!" });
+            (ActionGroupInvocationInput input, ILambdaContext context) => new ActionGroupInvocationOutput { Text = $"Hello, {input.Function}!" });
 
         var input = new ActionGroupInvocationInput { Function = "TestFunction" };
         var context = new TestLambdaContext();
@@ -118,7 +120,7 @@ public class BedrockAgentFunctionResolverTests
         // Arrange
         var resolver = new BedrockAgentFunctionResolver();
         resolver.Tool("TestFunction",
-            (input) => new ActionGroupInvocationOutput { Text = $"Hello, {input.Function}!" });
+            (ActionGroupInvocationInput input) => new ActionGroupInvocationOutput { Text = $"Hello, {input.Function}!" });
 
         var input = new ActionGroupInvocationInput { Function = "TestFunction" };
         var context = new TestLambdaContext();
@@ -208,23 +210,173 @@ public class BedrockAgentFunctionResolverTests
     {
         // Arrange
         var resolver = new BedrockAgentFunctionResolver();
-        resolver.Tool("sum_numbers", (payload, context ) =>
+        resolver.Tool(
+            name: "GetCustomForecast",
+            description: "Get detailed forecast for a location",
+            handler: (string location, int days, ILambdaContext ctx) => {
+                ctx.Logger.LogLine($"Getting forecast for {location}");
+                return $"{days}-day forecast for {location}";
+            }
+        );
+        
+        resolver.Tool(
+            name: "Greet",
+            description: "Greet a user",
+            handler: (string name) => {
+                return $"Hello {name}";
+            }
+        );
+        
+        resolver.Tool(
+            name: "Simple",
+            description: "Greet a user",
+            handler: () => {
+                return "Hello";
+            }
+        );
+        
+        var input = new ActionGroupInvocationInput
         {
-            
-            return new ActionGroupInvocationOutput { Text = "2" };
-        });
+            Function = "GetCustomForecast",
+            Parameters = new List<Parameter>
+            {
+                new Parameter
+                {
+                    Name = "location",
+                    Value = "Lisbon",
+                    Type = "String"
+                },
+                new Parameter
+                {
+                    Name = "days",
+                    Value = "1",
+                    Type = "Number"
+                }
+            }
+        };
         
         var context = new TestLambdaContext();
 
         // Act
-        var result = resolver.Resolve(_bedrockEvent, context);
+        var result = resolver.Resolve(input, context);
 
         // Assert
-        Assert.Equal("2", result.Text);
+        Assert.Equal("1-day forecast for Lisbon", result.Text);
+    }
+    
+    [Fact]
+    public void TestFunctionHandlerWithEventAndServices()
+    {
+        // Arrange
+        
+        // Setup DI
+        var services = new ServiceCollection();
+        services.AddSingleton(new HttpClient());
+        services.AddBedrockResolver();
+
+        var serviceProvider = services.BuildServiceProvider();
+        var resolver = serviceProvider.GetRequiredService<BedrockAgentFunctionResolver>();
+        
+        resolver.Tool(
+            name: "GetCustomForecast",
+            description: "Get detailed forecast for a location",
+            handler: async (string location, int days, HttpClient client, ILambdaContext ctx) =>
+            {
+                var resp = await client.GetStringAsync("https://api.open-meteo.com/v1/forecast?latitude=38.7167&longitude=-9.1333&current=temperature_2m");
+                return resp;
+            }
+        );
+        
+        resolver.Tool(
+            name: "Greet",
+            description: "Greet a user",
+            handler: (string name) => {
+                return $"Hello {name}";
+            }
+        );
+        
+        var input = new ActionGroupInvocationInput
+        {
+            Function = "GetCustomForecast",
+            Parameters = new List<Parameter>
+            {
+                new Parameter
+                {
+                    Name = "location",
+                    Value = "Lisbon",
+                    Type = "String"
+                },
+                new Parameter
+                {
+                    Name = "days",
+                    Value = "1",
+                    Type = "Number"
+                }
+            }
+        };
+        
+        var context = new TestLambdaContext();
+
+        // Act
+        var result = resolver.Resolve(input, context);
+
+        // Assert
+        Assert.Equal("1-day forecast for Lisbon", result.Text);
+    }
+    
+    [Fact]
+    public void TestFunctionHandlerWithEventTypes()
+    {
+        // Arrange
+        var resolver = new BedrockAgentFunctionResolver();
+        resolver.Tool(
+            name: "GetCustomForecast",
+            description: "Get detailed forecast for a location",
+            handler: (string location, int days, ILambdaContext ctx) => {
+                ctx.Logger.LogLine($"Getting forecast for {location}");
+                return $"{days}-day forecast for {location}";
+            }
+        );
+        
+        resolver.Tool(
+            name: "Greet",
+            description: "Greet a user",
+            handler: (string name) => {
+                return $"Hello {name}";
+            }
+        );
+        
+        var input = new ActionGroupInvocationInput
+        {
+            Function = "GetCustomForecast",
+            Parameters = new List<Parameter>
+            {
+                new Parameter
+                {
+                    Name = "location",
+                    Value = "Lisbon",
+                    Type = "String"
+                },
+                new Parameter
+                {
+                    Name = "days",
+                    Value = "1",
+                    Type = "Number"
+                }
+            }
+        };
+        
+        var context = new TestLambdaContext();
+
+        // Act
+        var result = resolver.Resolve(input, context);
+
+        // Assert
+        Assert.Equal("1-day forecast for Lisbon", result.Text);
     }
 }
 
-// Types
+
 // String
 // Number
 // Integer
