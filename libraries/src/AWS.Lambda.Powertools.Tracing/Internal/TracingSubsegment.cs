1c84183a52f0eba876f16a047fde0c5cb520cbbc
joaosusaki <jrabalem@gmail.com>
2025-08-04 18:18:34 -0300

    feat(tracing): add Subsegment wrapper to prevent exposing Amazon.XRay.Recorder.Core.Internal

---
+++ libraries/src/AWS.Lambda.Powertools.Tracing/Internal/TracingSubsegment.cs
@@ -0,0 +1,18 @@
+using Amazon.XRay.Recorder.Core.Internal.Entities;
+using System.Collections.Generic;
+
+namespace AWS.Lambda.Powertools.Tracing.Internal;
+
+/// <summary>
+///     Class TracingSubsegment.
+///     It's a wrapper for Subsegment from Amazon.XRay.Recorder.Core.Internal.
+/// </summary>
+/// <seealso cref="Subsegment" />
+public class TracingSubsegment : Subsegment
+{
+    /// <summary>
+    /// Wrapper constructor
+    /// </summary>
+    /// <param name="name"></param>
+    public TracingSubsegment(string name) : base(name) { }
+}
