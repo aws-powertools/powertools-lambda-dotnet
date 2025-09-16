fe51661efd640aa7eeb28808daf38165a8a4afd0
Henrique Graca <999396+hjgraca@users.noreply.github.com>
2025-09-16 15:36:16 +0100

    Revert "chore: initialize module initializers"
    This reverts commit b008221387593e2febd1d1612b5296abfa587846.

--- libraries/tests/AWS.Lambda.Powertools.SourceGenerator.Tests/UAGenerator/UASetter.cs
+++ libraries/tests/AWS.Lambda.Powertools.SourceGenerator.Tests/UAGenerator/UASetter.cs
@@ -4,26 +4,7 @@ using Xunit.Abstractions;
 
 namespace AWS.Lambda.Powertools.SourceGenerator.Tests;
 
-public class UATestFixture
-{
-    public UATestFixture()
-    {
-        // Initialize all utilities that have module initializers
-        BatchProcessing.Internal.UAModuleInitializer.Initialize();
-        EventHandler.Internal.UAModuleInitializer.Initialize();
-        EventHandler.Resolvers.BedrockAgentFunction.Internal.UAModuleInitializer.Initialize();
-        Idempotency.Internal.UAModuleInitializer.Initialize();
-        Kafka.Avro.Internal.UAModuleInitializer.Initialize();
-        Kafka.Json.Internal.UAModuleInitializer.Initialize();
-        Kafka.Protobuf.Internal.UAModuleInitializer.Initialize();
-        Logging.Internal.UAModuleInitializer.Initialize();
-        Metrics.Internal.UAModuleInitializer.Initialize();
-        Parameters.Internal.UAModuleInitializer.Initialize();
-        Tracing.Internal.UAModuleInitializer.Initialize();
-    }
-}
-
-public class UASetter : IClassFixture<UATestFixture>
+public class UASetter
 {
     private readonly ITestOutputHelper _output;
     private readonly string? _appId;
