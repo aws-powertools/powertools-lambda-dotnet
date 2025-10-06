a68967d278771619b014270aaf81b91e63f1fa1d
Henrique Graca <999396+hjgraca@users.noreply.github.com>
2025-10-06 21:12:01 +0100

    bring back extra.js

---
+++ docs/javascript/extra.js
@@ -0,0 +1,23 @@
+function copyToClipboard(e) {
+    e.preventDefault();
+    navigator.clipboard.writeText(e.target.textContent);
+    alert$.next('Copied to clipboard');
+}
+
+function enableClipboardElements() {
+    const copyElements = document.querySelectorAll('.copyMe');
+    copyElements.forEach((element) => {
+        element.addEventListener('click', copyToClipboard);
+    });
+}
+
+const attachListeners = () => {
+    enableSearchOnBlurElement();
+    enableClipboardElements();
+};
+
+const init = () => {
+    attachListeners();
+};
+
+init();
