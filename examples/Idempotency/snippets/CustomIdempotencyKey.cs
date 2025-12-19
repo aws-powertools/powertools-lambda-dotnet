0bb956bd943160c7eb529390fcbcda3a23bded3f
fuguikz <2514883828@qq.com>
2025-12-19 18:31:46 +0800

    docs: extract idempotency snippets

---
+++ examples/Idempotency/snippets/CustomIdempotencyKey.cs
@@ -0,0 +1,47 @@
+// This file is referenced by docs/utilities/idempotency.md
+// via pymdownx.snippets (mkdocs).
+
+// --8<-- [start:event_key_jmespath_payment]
+    Idempotency.Configure(builder =>
+            builder
+                .WithOptions(optionsBuilder =>
+                    optionsBuilder.WithEventKeyJmesPath("powertools_json(Body).[\"user_id\", \"product_id\"]"))
+                .UseDynamoDb("idempotency_table"));
+// --8<-- [end:event_key_jmespath_payment]
+
+// --8<-- [start:custom_key_prefix]
+public class Function
+{
+    public Function()
+    {
+        var tableName = Environment.GetEnvironmentVariable("IDEMPOTENCY_TABLE_NAME");
+        Idempotency.Configure(builder => builder.UseDynamoDb(tableName));
+    }
+
+    [Idempotent(KeyPrefix = "MyCustomKeyPrefix")]
+    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest apigwProxyEvent, ILambdaContext context)
+    {
+        return TestHelper.TestMethod(apigwProxyEvent);
+    }
+}
+// --8<-- [end:custom_key_prefix]
+
+// --8<-- [start:throw_on_no_idempotency_key]
+    public App() 
+    {
+      Idempotency.Configure(builder =>
+            builder
+                .WithOptions(optionsBuilder =>
+                    optionsBuilder
+                        // Requires "user"."uid" and "orderId" to be present
+                        .WithEventKeyJmesPath("[user.uid, orderId]")
+                        .WithThrowOnNoIdempotencyKey(true))
+                .UseDynamoDb("TABLE_NAME"));
+    }
+
+    [Idempotent]
+    public Task<OrderResult> FunctionHandler(Order input, ILambdaContext context)
+    {
+      // ...
+    }
+// --8<-- [end:throw_on_no_idempotency_key]
