8555a72077826f4d2e32064c978be7b81d11f47b
Henrique Graca <999396+hjgraca@users.noreply.github.com>
2026-03-18 19:37:01 +0000

    test(metadata): improve MetadataFetcher code coverage to 100%

--- libraries/tests/AWS.Lambda.Powertools.Metadata.Tests/Internal/MetadataFetcherTests.cs
+++ libraries/tests/AWS.Lambda.Powertools.Metadata.Tests/Internal/MetadataFetcherTests.cs
@@ -13,6 +13,7 @@
  * permissions and limitations under the License.
  */
 
+using System.Net;
 using AWS.Lambda.Powertools.Metadata.Exceptions;
 using AWS.Lambda.Powertools.Metadata.Internal;
 using FluentAssertions;
@@ -20,45 +21,162 @@ using Xunit;
 
 namespace AWS.Lambda.Powertools.Metadata.Tests.Internal;
 
-public class MetadataFetcherTests
+public class MetadataFetcherTests : IDisposable
 {
+    public MetadataFetcherTests()
+    {
+        Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_TOKEN", "test-token");
+        Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_API", "localhost:8080");
+    }
+
+    public void Dispose()
+    {
+        Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_TOKEN", null);
+        Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_API", null);
+    }
+
     [Fact]
     public void Fetch_ThrowsWhenTokenMissing()
     {
-        // Arrange
         Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_TOKEN", null);
-        Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_API", "localhost:8080");
 
-        try
-        {
-            var fetcher = new MetadataFetcher();
-            var act = () => fetcher.Fetch();
-            act.Should().Throw<LambdaMetadataException>()
-                .WithMessage("*AWS_LAMBDA_METADATA_TOKEN*");
-        }
-        finally
-        {
-            Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_API", null);
-        }
+        var fetcher = new MetadataFetcher();
+        var act = () => fetcher.Fetch();
+        act.Should().Throw<LambdaMetadataException>()
+            .WithMessage("*AWS_LAMBDA_METADATA_TOKEN*");
     }
 
     [Fact]
     public void Fetch_ThrowsWhenApiMissing()
     {
-        // Arrange
-        Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_TOKEN", "test-token");
         Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_API", null);
 
-        try
+        var fetcher = new MetadataFetcher();
+        var act = () => fetcher.Fetch();
+        act.Should().Throw<LambdaMetadataException>()
+            .WithMessage("*AWS_LAMBDA_METADATA_API*");
+    }
+
+    [Fact]
+    public void Fetch_ReturnsMetadata_WhenSuccessful()
+    {
+        var handler = new MockHandler(new HttpResponseMessage(HttpStatusCode.OK)
         {
-            var fetcher = new MetadataFetcher();
-            var act = () => fetcher.Fetch();
-            act.Should().Throw<LambdaMetadataException>()
-                .WithMessage("*AWS_LAMBDA_METADATA_API*");
-        }
-        finally
+            Content = new StringContent("{\"AvailabilityZoneID\":\"use1-az1\"}")
+        });
+        var fetcher = new MetadataFetcher(new HttpClient(handler));
+
+        var result = fetcher.Fetch();
+
+        result.AvailabilityZoneId.Should().Be("use1-az1");
+    }
+
+    [Fact]
+    public void Fetch_ThrowsWithStatusCode_WhenHttpError()
+    {
+        var handler = new MockHandler(new HttpResponseMessage(HttpStatusCode.Forbidden)
         {
-            Environment.SetEnvironmentVariable("AWS_LAMBDA_METADATA_TOKEN", null);
+            Content = new StringContent("access denied")
+        });
+        var fetcher = new MetadataFetcher(new HttpClient(handler));
+
+        var act = () => fetcher.Fetch();
+
+        act.Should().Throw<LambdaMetadataException>()
+            .Where(e => e.Message.Contains("403") && e.Message.Contains("access denied"))
+            .Where(e => e.StatusCode == 403);
+    }
+
+    [Fact]
+    public void Fetch_WrapsGenericException()
+    {
+        var handler = new MockHandler(new InvalidOperationException("connection refused"));
+        var fetcher = new MetadataFetcher(new HttpClient(handler));
+
+        var act = () => fetcher.Fetch();
+
+        act.Should().Throw<LambdaMetadataException>()
+            .WithMessage("*connection refused*")
+            .WithInnerException<InvalidOperationException>();
+    }
+
+    [Fact]
+    public void Fetch_ThrowsWhenDeserializationReturnsNull()
+    {
+        var handler = new MockHandler(new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent("null")
+        });
+        var fetcher = new MetadataFetcher(new HttpClient(handler));
+
+        var act = () => fetcher.Fetch();
+
+        act.Should().Throw<LambdaMetadataException>()
+            .WithMessage("*deserialize*");
+    }
+
+    [Fact]
+    public void Fetch_SetsAuthorizationHeader()
+    {
+        var handler = new MockHandler(new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent("{\"AvailabilityZoneID\":\"use1-az1\"}")
+        });
+        var fetcher = new MetadataFetcher(new HttpClient(handler));
+
+        fetcher.Fetch();
+
+        handler.LastRequest.Should().NotBeNull();
+        handler.LastRequest!.Headers.Authorization.Should().NotBeNull();
+        handler.LastRequest.Headers.Authorization!.Scheme.Should().Be("Bearer");
+        handler.LastRequest.Headers.Authorization.Parameter.Should().Be("test-token");
+    }
+
+    [Fact]
+    public void Fetch_UsesCorrectUrl()
+    {
+        var handler = new MockHandler(new HttpResponseMessage(HttpStatusCode.OK)
+        {
+            Content = new StringContent("{\"AvailabilityZoneID\":\"use1-az1\"}")
+        });
+        var fetcher = new MetadataFetcher(new HttpClient(handler));
+
+        fetcher.Fetch();
+
+        handler.LastRequest!.RequestUri!.ToString()
+            .Should().Be("http://localhost:8080/2026-01-15/metadata/execution-environment");
+    }
+
+    [Fact]
+    public void Fetch_RethrowsLambdaMetadataException()
+    {
+        var handler = new MockHandler(new LambdaMetadataException("original error"));
+        var fetcher = new MetadataFetcher(new HttpClient(handler));
+
+        var act = () => fetcher.Fetch();
+
+        act.Should().Throw<LambdaMetadataException>()
+            .WithMessage("original error")
+            .Where(e => e.InnerException == null);
+    }
+
+    private class MockHandler : HttpMessageHandler
+    {
+        private readonly HttpResponseMessage? _response;
+        private readonly Exception? _exception;
+        public HttpRequestMessage? LastRequest { get; private set; }
+
+        public MockHandler(HttpResponseMessage response) => _response = response;
+        public MockHandler(Exception exception) => _exception = exception;
+
+        protected override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            LastRequest = request;
+            if (_exception is not null) throw _exception;
+            return _response!;
         }
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            => Task.FromResult(Send(request, cancellationToken));
     }
 }
