ed9b2b2f2a4e422ba2bd588c21c1e6e50f4448be
Lasse Christiansen <lachriz@amazon.com>
2026-07-14 09:59:15 +0200

    docs(metrics): add documentation for AddDimensions method

---
+++ docs/snippets/metrics/AddingMultipleDimensions.cs
@@ -0,0 +1,22 @@
+// This file is referenced by docs/core/metrics.md
+// via pymdownx.snippets (mkdocs).
+
+namespace AWS.Lambda.Powertools.Docs.Snippets.Metrics;
+
+// --8<-- [start:adding_multiple_dimensions]
+using AWS.Lambda.Powertools.Metrics;
+
+public class Function {
+
+  [Metrics(Namespace = "ExampleApplication", Service = "Booking")]
+  public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest apigProxyEvent, ILambdaContext context)
+  {
+    Metrics.AddDimensions(
+        ("Environment", "Prod"),
+        ("Region", "eu-west-1")
+    );
+    Metrics.AddMetric("SuccessfulBooking", 1, MetricUnit.Count);
+    ...
+  }
+}
+// --8<-- [end:adding_multiple_dimensions]
